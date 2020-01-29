eb11fb3e4574b1cc12824cab1b4e88d0b5dc6f11
박태원 <producer82@naver.com>
2020-01-30 00:52:51 +0900

    알고리즘 완성

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from bs4 import BeautifulSoup
+from PyQt5.QtWidgets import QApplication, QWidget
 import urllib
 import requests
+import sys
 
+#공지를 제외시킨 게시글 리스트를 반환함
+def ignore_notice(title_list):
+    for i in title_list:
+        if i.find("em", {"class":"icon_notice"}) or i.find("span", {"class":"reply_num"}):
+            pass
+        else:
+            return i.text
+
+#첫 번째 글의 제목을 반환함
 def get_title():
-    gallery = requests.get('http://gall.dcinside.com/mgallery/board/lists/?id=apistogramma&page=1', headers = headers)
+    gallery = requests.get('http://gall.dcinside.com/mgallery/board/lists?id=aoegame', headers = headers)
     soup = BeautifulSoup(gallery.text, "html.parser")
     title_list = soup.select('.gall_list > tbody > tr > .gall_tit > a')
-    first_title = title_list[4].text
+    #반환할 때에 리스트에서 공지를 제외시킬 필요가 있음
+    return ignore_notice(title_list)
 
-    return first_title
-
+#메인
 if __name__ == "__main__":
     headers = {'Referer':'https://www.dcinside.com/',
     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"}
     before_title = get_title()
 
-    while(1):
-        if(before_title != get_title()):
-            print("새로운 글 올라옴")
-            before_title = get_title()
-
-
-    
+    print(get_title())
